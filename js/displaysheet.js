b869041f1f6fec5da5e4b4a533d51be985a52a92
Dewan <andewan@deloitte.nl>
2019-12-11 14:02:22 +0100

    files added

--- js/displaysheet.js
+++ js/displaysheet.js
@@ -1,6 +1,13 @@
 'use strict';
  
+/* Immediately-Invoked Function Expression (IIFE) executes immediately after it’s created.
+IIFE syntax : (function () { })(); */
 (function () {
+
+/* Syntax :  $(document ).ready(function () {});
+ready() makes a function available after the document is loaded. 
+Code written inside will run once the page DOM is ready to execute JS code. 
+The snippet can also be placed in <script> tags of html file for non JS related actions  */
   $(document).ready(function () {
     // Initialises Tableau Data Extension
     tableau.extensions.initializeAsync().then(function () {
@@ -15,4 +22,5 @@
         $("#worksheets").append("<button class='btn btn-secondary btn-block'>"+worksheet.name+"</button>");
     });
   }
+   
 })();
